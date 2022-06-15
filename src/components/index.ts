e1425589932738c1e64763da907ec772701eaff8
wangliuyuan <wangliuyuanperson@163.com>
2022-06-15 21:53:21 +0800

    modify:1、导出文件修改

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ import { wlyNotiFiction } from './Common/WlyNotification';
 import { WlyButton } from './Common/WlyButton';
 import { WlyTable } from './Common/WlyTable';
 import { WlyInput } from './Common/WlyInput';
-export { wlyNotiFiction, WlyButton, WlyTable, WlyInput };
+import { WlyForm, WlyFormItem } from './Common/WlyForm';
+import { WlyTabs, WlyTabPane } from './Common/WlyTabs';
+export { wlyNotiFiction, WlyButton, WlyTable, WlyInput, WlyForm, WlyFormItem, WlyTabs, WlyTabPane };
