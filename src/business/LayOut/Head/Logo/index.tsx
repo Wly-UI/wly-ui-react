2de555838708577c4e40d4ff1e5795cf6c2f21a0
wangliuyuan <wangliuyuanperson@163.com>
2022-06-15 21:49:06 +0800

    modify:1、目录结构改造

--- src/business/LayOut/Head/Logo/index.tsx
+++ src/business/LayOut/Head/Logo/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../../../../assets/image/logo.png';
+import logo from '../../../../assets/image/logo.png';
 import './index.less';
 const Logo: React.FC<any> = (props) => {
 	return (
